a89b96c2ac745bedb79d4ec7642aa0142bdaf4a5
VUONG <VUONG@VUONG>
2021-08-22 20:27:37 +0700

    Add project files.

---
+++ HocRevitAPI/Command.cs
@@ -0,0 +1,41 @@
+﻿#region Namespaces
+
+using Autodesk.Revit;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using Application = Autodesk.Revit.ApplicationServices.Application;
+
+#endregion
+
+namespace HocRevitAPI
+{
+    // Chỉ định từng phân đoạn cho Revit biết khi nào can thiệp, khi nào kết thúc để qua phân đoạn khác.
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    // IExternalCommand: Chứa các thủ tục để code liên kết với các thủ tục, đối tượng và tài nguyên trong Revit.
+    public class Command : IExternalCommand
+    {
+        // Thủ tục kết nối lệnh, chứa các đối số mặc định, thông qua đó Revit gửi cho ta những dữ liệu,
+        // tài nguyên của file Revit hay chương trình đang mở.
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData,
+            ref string message, Autodesk.Revit.DB.ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            // Code here
+            ICollection<ElementId> elementIds = uidoc.Selection.GetElementIds();
+
+            return Autodesk.Revit.UI.Result.Succeeded;  // Trả về kết quả thành công.
+        }
+    }
+}
---
+++ HocRevitAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HocRevitAPI")]
+[assembly: AssemblyDescription("Revit Add-In for HocRevitAPI")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Personal")]
+[assembly: AssemblyProduct("Revit C# .NET Add-In")]
+[assembly: AssemblyCopyright("Copyright © VuongLDT, Personal")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f0c0aa39-26dd-4e7b-b56c-8a1246c688f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
